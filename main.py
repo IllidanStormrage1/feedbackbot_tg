eeb41042005841a6b6e874f5b0141a0a2fb9026d
Kirill zudov <ueurhfyh1@gmail.com>
2019-07-02 16:03:53 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from aiogram import Bot
+from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from aiogram.utils.exceptions import Throttled
@@ -12,36 +12,34 @@ import aiohttp
 storage = MemoryStorage()
 loop = asyncio.get_event_loop()
 
-# PROXY_AUTH = aiohttp.BasicAuth(login='telegram', password='telegram')
-bot = Bot(config.token, loop=loop)
+PROXY_AUTH = aiohttp.BasicAuth(login='telegram', password='telegram')
+bot = Bot(config.token, proxy=config.PROXY_URL, loop=loop, proxy_auth=PROXY_AUTH)
 dp = Dispatcher(bot, storage=storage)
+
 
 @dp.message_handler(commands=["start"])
 async def process_start_command(msg):
-    ''' Приветствие /start '''
-    if msg.from_user.id in config.admins_id:
-        message = config.start_admin.format(msg.from_user.first_name)
+    try:
+        await dp.throttle('start', rate=1)
+    except Throttled:
+        pass
     else:
-        message = config.start.format(msg.from_user.first_name)
-    await bot.send_message(msg.from_user.id, message, disable_notification=True, parse_mode="Markdown")
-
+        await bot.send_message(msg.from_user.id, config.start.format(msg.from_user.first_name), disable_notification=True)
 
 @dp.message_handler(lambda msg: msg.reply_to_message and msg.from_user.id in config.admins_id)
-async def process_reply_admin(msg):
-    ''' Отправка пользователю '''
-    try:
-        await bot.send_message(msg.reply_to_message.forward_from.id, msg.text)
-        await bot.send_message(msg.from_user.id, config.succ_id)
-    except:
-        await bot.send_message(msg.from_user.id, config.error_id)
-
+async def process_reply(msg):
+    await bot.send_message(msg.reply_to_message.forward_from.id, msg.text)
 
 @dp.message_handler()
 async def process_send(msg):
-    ''' Отправка сообщения админам '''
-    for id in config.admins_id:
-        await bot.forward_message(id, msg.from_user.id, msg.message_id)
-    await bot.send_message(msg.from_user.id, config.repl, reply_to_message_id=msg.message_id, disable_notification=True)
+    try:
+        await dp.throttle('start', rate=1)
+    except Throttled:
+        pass1
+    else:
+        for id in config.admins_id:
+            await bot.forward_message(id, msg.from_user.id, msg.message_id)
+        await bot.send_message(msg.from_user.id, config.repl, reply_to_message_id=msg.message_id, disable_notification=True)
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True, loop=loop)
